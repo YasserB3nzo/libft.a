f304ba2dad452fbd061395f9fa6b6d964c2cca02
Yasser Benzidiya <165233802+YasserB3nzo@users.noreply.github.com>
2024-11-02 23:00:47 +0100

    Add files via upload

---
+++ ft_strrchr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcrhr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybenzidi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/02 20:06:16 by ybenzidi          #+#    #+#             */
+/*   Updated: 2024/11/02 20:06:17 by ybenzidi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+
+char	*ft_strrchr(const char *str, int c)
+{
+	int i = 0;
+	char *last_occurrence = NULL;
+
+	while (str[i])
+	{
+		if (str[i] == (char)c)
+		{
+			last_occurrence = (char *)&str[i];
+		}
+		i++;
+	}
+	if (c == '\0')
+		return ((char *)&str[i]);
+	return (last_occurrence);
+}
