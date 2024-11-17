d46bde963b01e9b66539355d5b537d5324d7ea45
Yasser Benzidiya <165233802+YasserB3nzo@users.noreply.github.com>
2024-11-17 12:08:53 +0100

    Add files via upload

--- ft_split.c
+++ ft_split.c
@@ -5,64 +5,71 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ybenzidi <ybenzidi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/16 13:56:00 by ybenzidi          #+#    #+#             */
-/*   Updated: 2024/11/16 13:56:00 by ybenzidi         ###   ########.fr       */
+/*   Created: 2024/11/16 23:59:52 by ybenzidi          #+#    #+#             */
+/*   Updated: 2024/11/17 12:00:01 by ybenzidi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int ft_count_words(char *array, char sep) {
-    int count;
-    int i;
+static int	ft_count_words(char *array, char sep)
+{
+	int	count;
+	int	i;
 
-    count = 0;
-    i = 0;
-    while (array[i]) {
-        while (array[i] == sep)
-            i++;
-        if (array[i] != '\0') 
-        {
-            count++;
-            while (array[i] && array[i] != sep)
-                i++;
-        }
-    }
-    return count;
+	count = 0;
+	i = 0;
+	while (array[i])
+	{
+		while (array[i] == sep)
+			i++;
+		if (array[i] != '\0')
+		{
+			count++;
+			while (array[i] && array[i] != sep)
+				i++;
+		}
+	}
+	return (count);
 }
 
 static char	*ft_copy_word(const char *s, int start, int end)
 {
-	char	*word = malloc((end - start + 1) * sizeof(char));
-	int		i = 0 ;
+	char	*word;
+	int		i;
+
+	i = 0;
+	word = malloc((end - start + 1) * sizeof(char));
 	if (!word)
-        return NULL;
+		return (NULL);
 	while (start < end)
 		word[i++] = s[start++];
-	word[i] = '\0'; 
+	word[i] = '\0';
 	return (word);
 }
 
-static void free_split(char **str)
+static void	free_split(char **str)
 {
-    int i;
-    i=0;
+	int	i;
+
+	i = 0;
 	while (str[i])
 	{
-        free(str[i]);
-        i++;
-    }
+		free(str[i]);
+		i++;
+	}
 	free(str);
 }
 
-char	**ft_split(char const *s, char c)
+static int	ft_fill_words(char **ptr, const char *s, char c, int count_words)
 {
-	int		count_words = ft_count_words((char *)s, c);
-	char	**ptr = malloc((count_words + 1) * sizeof(char *));
-	int		i = 0, word_index = 0, start, end;
+	int	i;
+	int	word_index;
+	int	start;
+	int	end;
 
-	if (!s || !ptr)
-        return NULL;
+	i = 0;
+	word_index = 0;
 	while (word_index < count_words)
 	{
 		while (s[i] == c)
@@ -71,26 +78,48 @@ char	**ft_split(char const *s, char c)
 		while (s[i] != c && s[i] != '\0')
 			i++;
 		end = i;
-		if (!(ptr[word_index++] = ft_copy_word(s, start, end)))
+		ptr[word_index] = ft_copy_word(s, start, end);
+		if (!ptr[word_index])
 		{
 			free_split(ptr);
-			return NULL;
+			return (0);
 		}
+		word_index++;
 	}
-	ptr[word_index] = NULL;
+	return (1);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int		count_words;
+	char	**ptr;
+
+	count_words = ft_count_words((char *)s, c);
+	if (!s)
+		return (NULL);
+	ptr = (char **) malloc ((count_words + 1) * sizeof (char *));
+	if (!ptr)
+		return (NULL);
+	if (!ft_fill_words(ptr, s, c, count_words))
+		return (NULL);
+	ptr[count_words] = NULL;
 	return (ptr);
 }
 
 /*#include<stdio.h>
-int main()
+int	main(void)
 {
    char car[] = "yasser$benzidiya$hafssa";
-   char **spli = ft_split("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultricies diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.", 'i'); 
+   char **spli = ft_split("lorem ipsum dolor sit amet,
+		consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor,
+		dignissim sit amet, adipiscing nec, ultricies sed,
+		dolor. Cras elementum ultricies diam. Maecenas ligula massa, varius a,
+		semper congue, euismod non, mi.", 'i');
    int i = 0;
    while (spli[i] != NULL)
    {
-       printf("spli[%d]: %s\n", i, spli[i]);
-       i++;
+		printf("spli[%d]: %s\n", i, spli[i]);
+		i++;
    }
-   return 0;
+   return (0);
 }*/
