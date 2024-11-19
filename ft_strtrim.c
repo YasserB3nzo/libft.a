165727fb493b8e01f1166fdcf656ed3622516455
Yasser Benzidiya <165233802+YasserB3nzo@users.noreply.github.com>
2024-11-20 00:13:57 +0100

    Update ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -10,36 +10,52 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azaki <azaki@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/26 22:32:29 by azaki             #+#    #+#             */
+/*   Updated: 2024/11/09 17:42:02 by azaki            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-static int	is_in_set(char c, const char *set)
+static int	in_set(int c, const char *set)
 {
+	if (!set)
+		return (0);
 	while (*set)
 	{
-		if (c == *set)
+		if (*set == c)
 			return (1);
 		set++;
 	}
 	return (0);
 }
 
-char	*ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(const char *s1, const char *set)
 {
-	int		start;
-	int		end;
-	char	*trimmed_str;
+	size_t	start;
+	size_t	end;
+	char	*new_string;
 
-	if (!s1 || !set)
+	if (!s1 && !set)
+		return (NULL);
+	if (!s1)
 		return (NULL);
 	start = 0;
-	while (s1[start] && is_in_set(s1[start], set))
-		start++;
 	end = ft_strlen(s1);
-	while (end > start && is_in_set(s1[end], set))
+	while (s1[start] && in_set(s1[start], set))
+		start++;
+	while (end > start && in_set(s1[end - 1], set))
 		end--;
-	trimmed_str = (char *)malloc(end - start + 1);
-	if (!trimmed_str)
+	new_string = malloc(sizeof(char) * (end - start + 1));
+	if (!new_string)
 		return (NULL);
-	ft_strlcpy(trimmed_str, s1 + start, end - start + 2);
-	return (trimmed_str);
+	ft_strlcpy(new_string, s1 + start, end - start + 1);
+	return (new_string);
 }
